aa94704adbc4ff2cc735be72a30d34d12d254f6d
Suren Baghdasaryan <surenb@google.com>
2023-12-27 08:51:53 +0300

    sm8250-common: Replace writepid with task_profiles command for cgroup migration
    writepid command usage to join a cgroup has been deprecated in favor
    of a more flexible approach using task_profiles. This way cgroup path
    is not hardcoded and cgroup changes can be easily made. Replace
    writepid with task_profiles command to migrate between cgroups.
    
    Bug: 191283136
    Test: build and boot
    Change-Id: I12fe7186af1ee5db6f19d485987e78ae0792c606
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- extract-files.py
+++ extract-files.py
@@ -63,6 +63,13 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
+    (
+        'vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc',
+        'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc',
+    ): blob_fixup()
+        .regex_replace('writepid /dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh'),
+    'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc': blob_fixup()
+        .regex_replace('writepid /dev/stune/nnapi-hal/tasks', 'task_profiles NNApiHALPerformance'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
